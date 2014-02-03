97eba006afb7cfcf3717a8cf6527ff3b34dcd1ea
Margaret Morris <margaretblue@gmail.com>
2014-02-02 23:07:55 -0800

    login logout destroy signin signup all working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,20 @@
 class WelcomeController < ApplicationController
+
+  before_filter :authorize, only: [:edit, :update]
+
   def index
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+
+
+
 end
